28d7b9987c6328ffa1607e03fb7572d335b8cadf
Shafiul Alam Khan Alvi <155105445+getalvi@users.noreply.github.com>
2026-05-27 20:29:03 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-{
-  "files": [],
-  "references": [
-    {
-      "path": "./tsconfig.app.json"
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { inspectAttr } from 'kimi-plugin-inspect-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [inspectAttr(), react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    {
-      "path": "./tsconfig.node.json"
-    }
-  ],
-  "compilerOptions": {
-    "baseUrl": ".",
-    "paths": {
-      "@/*": ["./src/*"]
-    }
-  }
-}
+  },
+});
