73a2922c6c72cccb30679b88324bedf9a140cbf1
Shafiul Alam Khan Alvi <155105445+getalvi@users.noreply.github.com>
2026-06-12 18:59:20 +0600

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,9 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://kkeacytrbrnundprstv.supabase.co";
-
-// নিচে ড্যাশবোর্ডের Settings > API থেকে কপি করা একদম নতুন Anon Key-টি বসান
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrZWFjeXRyYnJuZHVuZHByc3R2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MzY1MjUsImV4cCI6MjA5NjIxMjUyNX0.axvOFv8Lz4s9F4bW5xYHg3nor0CL9-lPdsNleZbP7cM"; 
+const supabaseUrl = "https://tmlnhsofgxjfchnxcooj.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtbG5oc29mZ3hqZmNobnhjb29qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNjQzNDAsImV4cCI6MjA5Njg0MDM0MH0.LsIZbHi60-dk1VpvtFvV54TJiBq4Z1GyPCiYpmMpAis";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
